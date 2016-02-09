6c67dc85ac06a09421bde363c47c5f534ecd725b
Mika92 <justthatgirl_l@mail.ru>
2016-02-09 17:51:40 +0200

    изменение2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[ ])
 QApplication app(argc, argv);
  
 QPushButton button("Hello, World!");
-button.resize(200, 60);
+button.resize(200, 60);50444
 button.show( );
  
 return app.exec( );
