5374689053996c0f42359657a89e37dae0fc98c8
Mika92 <justthatgirl_l@mail.ru>
2016-02-09 17:51:01 +0200

    изменение1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QPushButton>
+ 
+int main(int argc, char *argv[ ]) 
+{
+QApplication app(argc, argv);
+ 
+QPushButton button("Hello, World!");
+button.resize(200, 60);
+button.show( );
+ 
+return app.exec( );
+}
